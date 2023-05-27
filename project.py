b03ee395742e82a824f242b56bb43143d05f1c74
charlesbenedict07 <134706209+charlesbenedict07@users.noreply.github.com>
2023-05-27 13:19:19 +0800

    project.py

--- project.py
+++ project.py
@@ -1,14 +1,16 @@
-def load_model():
-  model=tf.keras.models.load_model('project.h5')
-  return model
+import streamlit as st
+import tensorflow as tf
 
-model = load_model()
+@st.cache(allow_output_mutation=True, hash_funcs={tf.keras.models.Model: id})
+def load_model():
+    model = tf.keras.models.load_model('project.h5')
+    return model
 
 st.write("""
-# Weather classifier
+# Weather Image Classifier
 """)
 
-file = st.file_uploader("Choose weather photo from computer", type=["jpg", "png"])
+file = st.file_uploader("upload any weather image", type=["jpg", "png"])
 
 import cv2
 from PIL import Image, ImageOps
@@ -19,6 +21,8 @@ def import_and_predict(image_data, model):
     image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
     img = np.asarray(image)
     img_reshape = img[np.newaxis, ...]
+    # Preprocess the image if required
+    # img_preprocessed = preprocess_image(img_reshape)
     prediction = model.predict(img_reshape)
     return prediction
 
@@ -27,7 +31,9 @@ if file is None:
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
+    model = load_model()
     prediction = import_and_predict(image, model)
-    class_names = ['Cloudy', 'Rain', 'Shine', 'Sunrise']
-    string = "OUTPUT : " + class_names[np.argmax(prediction)]
+    
+    class_names =  ['Cloudy', 'Rain', 'Shine', 'Sunrise']
+    string = "OUTPUT: " + class_names[np.argmax(prediction)]
     st.success(string)
